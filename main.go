4586539142a18756233ec85dd39b1dd9a73dffb1
zls <jszls65@qq.com>
2023-08-22 11:29:25 +0800

    添加创建 destination 接口

--- main.go
+++ main.go
@@ -62,6 +62,25 @@ func main() {
 			"data": policy,
 		})
 	})
+
+	// 创建sqs destination
+	r.GET("subscribe/destinations", func(c *gin.Context) {
+		shopName := c.Query("shopName")
+		// 参数校验
+		if shopName == "" {
+			c.JSON(400, gin.H{
+				"data": "参数异常",
+			})
+			return
+		}
+		// 发送http请求
+		destinations := subfunc.CreateDestinations(shopName)
+		c.JSON(200, gin.H{
+			"msg":  "执行成功",
+			"data": destinations,
+		})
+	})
+
 	err := r.Run(":8082")
 	common.HandleError(err)
 }
